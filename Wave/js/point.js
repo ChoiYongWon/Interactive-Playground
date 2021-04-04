8d2b161445f88b0a10dcd8c60ed4f502cc6b73c7
ChoiYongWon <yongwon0824@naver.com>
2021-04-04 17:14:31 +0900

    :zap:Wave Interaction

---
+++ Wave/js/point.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Yongwon on 2021-04-04
+ * Email: yongwon0824@naver.com
+ */
+
+export class Point {
+    constructor(index, x, y) {
+        this.x = x;
+        this.y = y;
+        this.fixedY = y;
+        this.speed = 0.1;
+        this.cur = index;
+        this.max = Math.random() * 100 + 150;
+    }
+
+    update(){
+        this.cur += this.speed;
+        this.y = this.fixedY + (Math.sin(this.cur) * this.max);
+    }
+}
